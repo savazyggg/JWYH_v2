bbd8dcb796b1050c239dd271c2676bc46d14bf2c
yun seo choi <ywest1280@gmail.com>
2023-05-31 21:06:13 +0900

    [Design] 인트로 페이지 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,28 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
+import styled from "@emotion/styled";
+import CssBaseline from "@mui/material/CssBaseline";
+import Box from "@mui/material/Box";
+import Container from "@mui/material/Container";
+import Button from "@mui/material/Button";
+
 import "./App.css";
 
-function App() {
-  const [count, setCount] = useState(0);
+const Greetings = styled.label`
+  font-size: 50px;
+`;
 
+function App() {
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Greetings>만나서 반가워요!</Greetings>
+      <CssBaseline />
+      <Container maxWidth="sm">
+        <Box sx={{ bgcolor: "#cfe8fc", width: "360px", height: "360px" }}>
+          <span>편지</span>
+        </Box>
+      </Container>
+      <Button>로그인</Button>
+      <Button>회원가입</Button>
     </>
   );
 }
