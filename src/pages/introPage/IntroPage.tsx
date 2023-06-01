1c533ed92672f1abd7090d7d68fbcc6ad22bd359
yun seo choi <ywest1280@gmail.com>
2023-06-01 16:52:43 +0900

    [Design] 인트로 페이지 추가

---
+++ src/pages/introPage/IntroPage.tsx
@@ -0,0 +1,87 @@
+import { useState } from "react";
+import React from "react";
+import CssBaseline from "@mui/material/CssBaseline";
+import Typography from "@mui/material/Typography";
+import Box from "@mui/material/Box";
+import Container from "@mui/material/Container";
+import Button from "@mui/material/Button";
+import Grid from "@mui/material/Grid";
+
+import "./IntroPage.css";
+import SignUpForm from "../../components/organisms/signUpForm/SignUpForm";
+import LoginForm from "../../components/organisms/loginForm/LoginForm";
+
+/* 스타일드 컴포넌트 코드
+ * 폰트 사이즈
+ */
+// const Greetings = styled.label`
+//   font-size: 50px;
+// `;
+//
+
+function IntroPage() {
+  const [isLogin, setIsLogin] = useState<boolean>(false);
+  const [isSignUp, setIsSignUp] = useState<boolean>(false);
+  const [greetings, setGreetings] = useState<string>("만나서 반가워요!!");
+  return (
+    <>
+      <Typography component="h1" variant="h4">
+        {greetings}
+      </Typography>
+      {isLogin || isSignUp ? null : (
+        <>
+          <CssBaseline />
+          <Container maxWidth="sm">
+            <Box sx={{ bgcolor: "#cfe8fc", width: "360px", height: "360px" }}>
+              <span>편지</span>
+            </Box>
+          </Container>
+
+          <Grid container spacing={2}>
+            <Grid item xs={6}>
+              <Button
+                type="button"
+                fullWidth
+                variant="contained"
+                sx={{ mt: 3, mb: 2 }}
+                onClick={() => {
+                  setIsLogin(!isLogin);
+                }}
+              >
+                로그인
+              </Button>
+            </Grid>
+            <Grid item xs={6}>
+              <Button
+                type="button"
+                fullWidth
+                variant="contained"
+                sx={{ mt: 3, mb: 2 }}
+                onClick={() => {
+                  setIsSignUp(!isSignUp);
+                }}
+              >
+                회원가입
+              </Button>
+            </Grid>
+          </Grid>
+        </>
+      )}
+      <>
+        {isSignUp ? (
+          <SignUpForm
+            greetings={greetings}
+            setGreetings={setGreetings}
+            isSignUp={isSignUp}
+            setIsSignUp={setIsSignUp}
+          />
+        ) : null}
+        {isLogin ? (
+          <LoginForm greetings={greetings} setGreetings={setGreetings} />
+        ) : null}
+      </>
+    </>
+  );
+}
+
+export default IntroPage;
