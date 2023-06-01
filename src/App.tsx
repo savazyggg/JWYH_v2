282ba78ea211efeae2fcc0ec34eab667d23437ed
yun seo choi <ywest1280@gmail.com>
2023-06-01 16:49:40 +0900

    [Feat] 라우팅 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,11 @@
-import { useState } from "react";
-import styled from "@emotion/styled";
-import CssBaseline from "@mui/material/CssBaseline";
-import Box from "@mui/material/Box";
-import Container from "@mui/material/Container";
-import Button from "@mui/material/Button";
-
+import { Routes, Route } from "react-router-dom";
+import IntroPage from "./pages/introPage/IntroPage";
 import "./App.css";
-
-const Greetings = styled.label`
-  font-size: 50px;
-`;
-
 function App() {
   return (
-    <>
-      <Greetings>만나서 반가워요!</Greetings>
-      <CssBaseline />
-      <Container maxWidth="sm">
-        <Box sx={{ bgcolor: "#cfe8fc", width: "360px", height: "360px" }}>
-          <span>편지</span>
-        </Box>
-      </Container>
-      <Button>로그인</Button>
-      <Button>회원가입</Button>
-    </>
+    <Routes>
+      <Route path="/" element={<IntroPage />} />
+    </Routes>
   );
 }
 
--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
+import { BrowserRouter } from "react-router-dom";
 import { RecoilRoot } from "recoil";
 import "@fontsource/roboto/300.css";
 import "@fontsource/roboto/400.css";
@@ -9,7 +10,9 @@ import "@fontsource/roboto/500.css";
 import "@fontsource/roboto/700.css";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
-  <RecoilRoot>
-    <App />
-  </RecoilRoot>
+  <BrowserRouter>
+    <RecoilRoot>
+      <App />
+    </RecoilRoot>
+  </BrowserRouter>
 );
