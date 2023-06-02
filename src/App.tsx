8ecdbaa32564ce1b05bf1bc8828fb6cd681ff299
yun seo choi <ywest1280@gmail.com>
2023-06-02 17:31:15 +0900

    [Test] 편지쓰기 라우팅 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,13 @@
 import { Routes, Route } from "react-router-dom";
 import IntroPage from "./pages/introPage/IntroPage";
+import WritingLetter from "./pages/writingLetter/WritingLetter";
+
 function App() {
   return (
     <Routes>
       <Route path="/" element={<IntroPage />} />
+      <Route path="/writingletter" element={<WritingLetter />} />
+      {/* <Route Path="/letterspace" element={<LetterSpace/>} /> */}
     </Routes>
   );
 }
