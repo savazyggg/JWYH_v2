4d9052ad098b80f72a9079383077387c6c0d8d8e
yun seo choi <ywest1280@gmail.com>
2023-06-02 18:11:32 +0900

    [Design] 글로벌 디자인 적용

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,24 @@
+import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import { RecoilRoot } from "recoil";
+import CssBaseline from "@mui/material/CssBaseline";
+import "@fontsource/roboto/300.css";
+import "@fontsource/roboto/400.css";
+import "@fontsource/roboto/500.css";
+import "@fontsource/roboto/700.css";
 import { Routes, Route } from "react-router-dom";
 import IntroPage from "./pages/introPage/IntroPage";
 import WritingLetter from "./pages/writingLetter/WritingLetter";
-
-function App() {
-  return (
-    <Routes>
-      <Route path="/" element={<IntroPage />} />
-      <Route path="/writingletter" element={<WritingLetter />} />
-      {/* <Route Path="/letterspace" element={<LetterSpace/>} /> */}
-    </Routes>
-  );
-}
-
-export default App;
+import "./App.css";
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
+  <BrowserRouter>
+    <CssBaseline />
+    <RecoilRoot>
+      <Routes>
+        <Route path="/" element={<IntroPage />} />
+        <Route path="/writingletter" element={<WritingLetter />} />
+        {/* <Route Path="/letterspace" element={<LetterSpace/>} /> */}
+      </Routes>
+    </RecoilRoot>
+  </BrowserRouter>
+);
