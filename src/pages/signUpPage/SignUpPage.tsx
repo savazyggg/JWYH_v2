e17332f84dbeb87c3207ec7fd7c01dd0082855c5
[SW 4기] 최윤서 <ywest1280@gmail.com>
2023-06-07 17:03:38 +0900

    [Design] 회원가입 페이지 stack 으로 정렬

--- src/pages/signUpPage/SignUpPage.tsx
+++ src/pages/signUpPage/SignUpPage.tsx
@@ -1,6 +1,7 @@
 import Typography from "@mui/material/Typography";
 import Container from "@mui/material/Container";
 import SignUpForm from "../../components/organisms/signUpForm/SignUpForm";
+import Stack from "@mui/material/Stack";
 function SignUpPage() {
   return (
     <>
@@ -8,11 +9,18 @@ function SignUpPage() {
         maxWidth="sm"
         style={{ display: "flex", justifyContent: "center" }}
       >
-        <Typography component="h1" variant="h4" style={{ color: "#93BA7B" }}>
-          회원가입!!"
-        </Typography>
+        <Stack direction={"column"} spacing={2}>
+          <Typography
+            sx={{ width: "100%" }}
+            component="h1"
+            variant="h4"
+            style={{ color: "#93BA7B" }}
+          >
+            회원가입!!"
+          </Typography>
+          <SignUpForm />
+        </Stack>
       </Container>
-      <SignUpForm />
     </>
   );
 }
