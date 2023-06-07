dcff4950f86c0d49594cde827c10b36f8bad0ea9
[SW 4기] 최윤서 <ywest1280@gmail.com>
2023-06-07 14:30:38 +0900

    [Refactor] 로그인페이지, 회원가입페이지 라우팅 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, Outlet, Link } from "react-router-dom";
 import { RecoilRoot } from "recoil";
+import { ThemeProvider, createTheme } from "@mui/material/styles";
 import CssBaseline from "@mui/material/CssBaseline";
 import "@fontsource/roboto/300.css";
 import "@fontsource/roboto/400.css";
@@ -11,22 +12,35 @@ import IntroPage from "./pages/introPage/IntroPage";
 import WritingLetterPage from "./pages/writingLetter/WritingLetterPage";
 import "./App.css";
 import MainPage from "./pages/mainPage/MainPage";
-import SignUpForm from "./components/organisms/signUpForm/SignUpForm";
-import LoginForm from "./components/organisms/loginForm/LoginForm";
+import ErrorPage from "./pages/errorPage/ErrorPage";
+import SignUpPage from "./pages/signUpPage/SignUpPage";
+import LoginPage from "./pages/loginPage/LoginPage";
+import { ThemeContext } from "@emotion/react";
+
+const darkTheme = createTheme({
+  palette: {
+    mode: "dark",
+  },
+});
 
 //TODO 06.03 윤지 : 라우팅 테스트용으로 임시로 만듬, 경로 경우의수 산정하여 리다이렉트 등 기능 넣을것
 //TODO 06.03 윤지 : 로그인, 회원가입 인트로페이지에서 props을 받던데 라우팅 할때 어떻게 할지? - 헤더에서 (링크걸어야됨) 윤서님과 확인하기
+
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <BrowserRouter>
-    <CssBaseline />
-    <RecoilRoot>
-      <Routes>
-        <Route path="/" element={<IntroPage />} />
-        <Route path="/writingletter" element={<WritingLetterPage />} />
-        <Route path="/main" element={<MainPage />} />
-        {/* <Route path="/login" element={<LoginForm />} />
-        <Route path="/signup" element={<SignUpForm />} /> */}
-      </Routes>
-    </RecoilRoot>
+    <ThemeProvider theme={darkTheme}>
+      <CssBaseline />
+      <RecoilRoot>
+        <Routes>
+          <Route path="/" element={<IntroPage />} />
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/signup" element={<SignUpPage />} />
+          <Route path="/writingletter" element={<WritingLetterPage />} />
+          <Route path="/main" element={<MainPage />} />
+          {/* 위 주소를 제외한 모든 주소는 error 페이지로 */}
+          <Route path="*" element={<ErrorPage />} />
+        </Routes>
+      </RecoilRoot>
+    </ThemeProvider>
   </BrowserRouter>
 );
