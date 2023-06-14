3761746520420b342e413574e009ae78e554bd6e
[SW 4기] 최윤서 <ywest1280@gmail.com>
2023-06-14 13:03:56 +0900

    [Feat] recoil-persist 추가

--- src/recoilStore.ts
+++ src/recoilStore.ts
@@ -1,38 +1,47 @@
 import { atom, selector } from "recoil";
+import { recoilPersist } from "recoil-persist";
+
+const { persistAtom } = recoilPersist();
 
 //로그인 상태
 const isLoginedState = atom({
   key: "isLogined",
   default: false,
+  effects_UNSTABLE: [persistAtom],
 });
 
 //난수 아이디
 const uniqueIdState = atom({
   key: "uniqueId",
   default: "",
+  effects_UNSTABLE: [persistAtom],
 });
 
 //유저 아이디
 const userIdState = atom({
   key: "userId",
   default: "",
+  effects_UNSTABLE: [persistAtom],
 });
 
 //유저 닉네임
 const nickNameState = atom({
   key: "nickName",
   default: "",
+  effects_UNSTABLE: [persistAtom],
 });
 
 //jwt
 const jwtStringState = atom({
   key: "jwtStringState",
   default: "",
+  effects_UNSTABLE: [persistAtom],
 });
 //소셜 로그인
 const providerState = atom({
   key: "providerState",
   default: "",
+  effects_UNSTABLE: [persistAtom],
 });
 
 export {
