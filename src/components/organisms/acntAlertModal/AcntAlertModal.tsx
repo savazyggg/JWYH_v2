1899ef1dea6ec890f079513d5cb928d9a8223af7
[SW 4기] 최윤서 <ywest1280@gmail.com>
2023-06-15 16:16:19 +0900

    [Design] 마이페이지 모달 디자인 수정

--- src/components/organisms/acntAlertModal/AcntAlertModal.tsx
+++ src/components/organisms/acntAlertModal/AcntAlertModal.tsx
@@ -10,9 +10,10 @@ const style = {
   left: "50%",
   transform: "translate(-50%, -50%)",
   width: 400,
-  bgcolor: "background.paper",
+  bgcolor: "rgb(63, 63, 63)",
   border: "2px solid #000",
   boxShadow: 24,
+  borderRadius: "10px",
   p: 4,
 };
 
@@ -36,7 +37,12 @@ export default function AlertModal(props: props) {
       aria-describedby="modal-modal-description"
     >
       <Box sx={style}>
-        <Typography id="modal-modal-title" variant="h6" component="h2">
+        <Typography
+          sx={{ color: "white" }}
+          id="modal-modal-title"
+          variant="h6"
+          component="h2"
+        >
           {modalMsg}
         </Typography>
         <Button
