4c32e6d35cdcbbe79b6dcfb1e30c6f1dd73fe0ca
[SW 4기] 최윤서 <ywest1280@gmail.com>
2023-06-16 02:45:25 +0900

    [Design] 편지 애니메이션 수정

--- src/components/organisms/envelope/Envelope.tsx
+++ src/components/organisms/envelope/Envelope.tsx
@@ -1,51 +1,12 @@
-import { useEffect, useRef } from "react";
 import "./envelope.css";
 const Envelope = () => {
-  const envelopeRef = useRef<HTMLDivElement>(null);
-  const envelopeTopRef = useRef<HTMLDivElement>(null);
-  const letterRef = useRef<HTMLDivElement>(null);
-
-  const openEnvelopeOnHover = () => {
-    envelopeTopRef.current?.classList.remove("close");
-    envelopeTopRef.current?.classList.add("open");
-
-    pullOutPartial();
-  };
-
-  const pullOutPartial = () => {
-    letterRef.current?.classList.remove("in");
-    letterRef.current?.classList.add("out-partial");
-  };
-
-  const closeEnvelopeOnHover = () => {
-    putIn();
-    setTimeout(function () {
-      envelopeTopRef.current?.classList.remove("open");
-      envelopeTopRef.current?.classList.add("close");
-    }, 400);
-  };
-
-  const putIn = () => {
-    letterRef.current?.classList.remove("out-partial");
-    letterRef.current?.classList.add("in");
-  };
-
-  useEffect(() => {
-    envelopeRef.current?.addEventListener("mouseover", () => {
-      openEnvelopeOnHover();
-    });
-    envelopeRef.current?.addEventListener("mouseout", () => {
-      closeEnvelopeOnHover();
-    });
-  }, []);
-
   return (
     <>
-      <div ref={envelopeRef} className="envelope">
-        <div ref={envelopeTopRef} className="cover top close"></div>
+      <div className="envelope envelopeLetterCarousel ">
+        <div className="cover top  loopLetterAnimation"></div>
         <div className="cover bottom right"></div>
         <div className="cover left"></div>
-        <div ref={letterRef} className="introLetter"></div>
+        <div className="introLetter loopPartOutNInAnimation"></div>
       </div>
     </>
   );
