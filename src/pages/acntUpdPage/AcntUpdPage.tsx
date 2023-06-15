5d5c3fd71aded18156dc037e65616e315e620df9
[SW 4기] 최윤서 <ywest1280@gmail.com>
2023-06-15 16:11:16 +0900

    [Design] 마이페이지 색 수정

--- src/pages/acntUpdPage/AcntUpdPage.tsx
+++ src/pages/acntUpdPage/AcntUpdPage.tsx
@@ -4,7 +4,7 @@ import Container from "@mui/material/Container";
 import AcntUpdForm from "../../components/organisms/acntUpdForm/AcntUpdForm";
 import Stack from "@mui/material/Stack";
 import Header from "../../components/organisms/header/Header";
-import { Box } from "@mui/material";
+import { Box, createTheme, ThemeProvider } from "@mui/material";
 import { useNavigate } from "react-router-dom";
 import jwt_decode from "jwt-decode";
 
@@ -17,21 +17,62 @@ interface JwtDecoded {
   objectId: string;
 }
 function AcntUpdPage() {
+  const theme = createTheme({
+    palette: {
+      primary: {
+        main: "#93ba7b",
+      },
+    },
+    components: {
+      // Name of the component ⚛️
+      MuiInputLabel: {
+        styleOverrides: {
+          root: {
+            color: "#fff",
+          },
+        },
+      },
+      MuiFormHelperText: {
+        styleOverrides: {
+          root: {
+            color: "#fff",
+          },
+        },
+      },
+      MuiButton: {
+        styleOverrides: {
+          root: {
+            color: "#fff",
+          },
+        },
+      },
+      MuiOutlinedInput: {
+        styleOverrides: {
+          root: {
+            color: "#fff",
+            backgroundColor: "#3f3f3f",
+          },
+        },
+      },
+    },
+  });
   const [isLogined, setIsLogined] = useRecoilState(isLoginedState);
 
   return (
     <>
-      <Container
-        maxWidth="sm"
-        style={{ display: "flex", justifyContent: "center" }}
-      >
-        <Stack direction={"column"} spacing={2}>
-          <Box sx={{ height: "50px" }}>
-            <Header></Header>
-          </Box>
-          {isLogined && <AcntUpdForm />}
-        </Stack>
-      </Container>
+      <ThemeProvider theme={theme}>
+        <Container
+          maxWidth="sm"
+          style={{ display: "flex", justifyContent: "center" }}
+        >
+          <Stack direction={"column"} spacing={2}>
+            <Box sx={{ height: "50px" }}>
+              <Header></Header>
+            </Box>
+            {isLogined && <AcntUpdForm />}
+          </Stack>
+        </Container>
+      </ThemeProvider>
     </>
   );
 }
